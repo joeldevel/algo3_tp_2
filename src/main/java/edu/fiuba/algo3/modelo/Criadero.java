62e0fc40b1f97a221c9c767de10eda6b33a0aa55
brianlahuta <67164914+brianlahuta@users.noreply.github.com>
2022-11-06 11:28:53 -0300

    Clases Criadero y Larva, y primera prueba de Criader

--- src/main/java/edu/fiuba/algo3/modelo/Criadero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Criadero.java
@@ -1,15 +1,22 @@
 package edu.fiuba.algo3.modelo;
+import java.util.*;
 
 public class Criadero {
 	
 	private int maxLarvas;
+	private ArrayList<Larva> larvas;
 	
 	public Criadero() {
 		this.maxLarvas = 3;
 	}
 	
+	public Criadero(ArrayList<Larva>unasLarvas) {
+		this.maxLarvas = 3;
+		this.larvas = unasLarvas;
+	}
+	
 	public int contarLarvas() {
-		return this.maxLarvas;
+		return this.larvas.size();
 	}
 
 }
--- src/main/test/edu/fiuba/algo3/entrega_1/PruebasCriadero.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/PruebasCriadero.java
@@ -2,16 +2,28 @@ package edu.fiuba.algo3.entrega_1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 import edu.fiuba.algo3.modelo.Criadero;
+import edu.fiuba.algo3.modelo.Larva;
 
 class PruebasCriadero {
 
 	@Test
 	void test01UnNuevoCriaderoNuevoDeberiaEmpezarConTresLarvas(){
 		
-		Criadero criadero = new Criadero();
+		/* la parte del ciclo debería estar en otra parte
+		 * puesto que al crear el criadero ya deberia contener las 3 larvas.
+		 * Quizá es una interfaz que implementa Criadero*/
+		
+		ArrayList<Larva>larvas = new ArrayList<Larva>();
+		for(int i=0; i<3; i++) {
+			larvas.add(new Larva());
+		}
+		
+		Criadero criadero = new Criadero(larvas);
 		
 		assertEquals(criadero.contarLarvas(),3);
 		
