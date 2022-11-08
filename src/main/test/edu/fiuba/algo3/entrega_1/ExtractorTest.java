0db7f1528ac3a424fd754612d89efd7519aa1b0d
AlanValdevenito <avaldevenito@fi.uba.ar>
2022-11-07 21:45:59 -0300

    Caso de uso 4 (sin refactor) completo.

--- src/main/test/edu/fiuba/algo3/entrega_1/ExtractorTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/ExtractorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ExtractorTest {
+class ExtractorTest {
 
     @Test
     void test01SeConstruyeUnExtractorEnUnVolcanYNoSeEncuentraOperativo(){
