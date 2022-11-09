a91b39f815c551e7bd777645242b57f285b21086
joeldevel <christianjoel.developer@gmail.com>
2022-11-09 20:19:17 -0300

    Crea clase para testear recolección de minerales

---
+++ src/main/test/edu/fiuba/algo3/entrega_1/RecoleccionMineralesTest.java
@@ -0,0 +1,24 @@
+package edu.fiuba.algo3.entrega_1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class RecoleccionMineralesTest {
+
+/*
+* Cada zángano puede trabajar sobre 1 mineral a la vez, recogiendo 10 unidades de
+mineral por turno.
+* */
+    @Test
+    @DisplayName("Un zangano extrae 10 unidades de mineral por turno")
+    public void unZanganoExtrae10UnidadesDeMineralPorTurno() {
+        // mock de un mineral
+        // poner la larva sobre el mineral
+        // pasar 1 turno
+        // verificar que el mineral tiene -10 cantidad
+        // verificar que el zangano de alguna manera saca el mineral
+    }
+
+
+}
