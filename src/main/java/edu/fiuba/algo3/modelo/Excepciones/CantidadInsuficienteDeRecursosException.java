c0e22859b3cd31a04f54c972cb7368b909e6f923
joeldevel <christianjoel.developer@gmail.com>
2022-11-10 16:30:59 -0300

    Se crea excepción para cantidad de recursos insuficientes

---
+++ src/main/java/edu/fiuba/algo3/modelo/Excepciones/CantidadInsuficienteDeRecursosException.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.Excepciones;
+
+public class CantidadInsuficienteDeRecursosException extends RuntimeException {
+    public CantidadInsuficienteDeRecursosException(String msg) {
+        super(msg);
+    }
+}
