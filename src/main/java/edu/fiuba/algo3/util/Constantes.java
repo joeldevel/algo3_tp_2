7c1b66a928a160403a894750dc7eb819f966d7c5
joeldevel <christianjoel.developer@gmail.com>
2022-11-10 19:25:10 -0300

    constantes para edificios zerg

--- src/main/java/edu/fiuba/algo3/util/Constantes.java
+++ src/main/java/edu/fiuba/algo3/util/Constantes.java
@@ -7,4 +7,12 @@ public class Constantes {
     public static final int COSTO_MINERAL_PUERTO = 150;
     public static final int COSTO_GAS_PUERTO = 150;
     public static final int COSTO_NEXO = 50;
+
+    public static final int COSTO_MINERAL_CRIADERO = 50;
+    public static final int COSTO_MINERAL_EXTRACTOR = 100;
+    public static final int COSTO_MINERAL_RESERVA_PRODUCCION = 150;
+    public static final int COSTO_MINERAL_GUARIDA = 200;
+    public static final int COSTO_GAS_GUARIDA = 100;
+    public static final int COSTO_MINERAL_ESPIRAL = 150;
+    public static final int COSTO_GAS_ESPIRAL = 100;
 }
