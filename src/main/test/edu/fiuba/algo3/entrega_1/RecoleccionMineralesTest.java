21a92c87c91da77a890134ebfecbcc97dcdc8ae0
joeldevel <christianjoel.developer@gmail.com>
2022-11-10 01:36:36 -0300

    tests para el caso 7 - recolección de minerales

--- src/main/test/edu/fiuba/algo3/entrega_1/RecoleccionMineralesTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/RecoleccionMineralesTest.java
@@ -1,24 +1,65 @@
 package edu.fiuba.algo3.entrega_1;
 
+import edu.fiuba.algo3.modelo.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public class RecoleccionMineralesTest {
 
-/*
-* Cada zángano puede trabajar sobre 1 mineral a la vez, recogiendo 10 unidades de
-mineral por turno.
-* */
     @Test
-    @DisplayName("Un zangano extrae 10 unidades de mineral por turno")
-    public void unZanganoExtrae10UnidadesDeMineralPorTurno() {
-        // mock de un mineral
-        // poner la larva sobre el mineral
-        // pasar 1 turno
-        // verificar que el mineral tiene -10 cantidad
-        // verificar que el zangano de alguna manera saca el mineral
+    @DisplayName("Un zángano extrae 10 unidades de mineral en 1 turno ")
+    public void extraccionDeMineralConUnZanganoTest() {
+
+        NodoMineral nodoMineral = new NodoMineral(new Mineral(10));
+        Zangano zangano = new Zangano();
+        zangano.setCantidadMineralExtraidoPorTurno(10);
+        zangano.recibirMineral(nodoMineral.getMineral());
+        zangano.avanzarTurno();
+        Assertions.assertEquals(10, zangano.devolverMineralExtraido());
     }
 
+    @Test
+    @DisplayName("Un zángano extrae 0 al extraer un nodo mineral agotado")
+    public void extraccionDeMineralConUnZanganoNodoAgotadoTest() {
+
+        NodoMineral nodoMineral = new NodoMineral(new Mineral(0));
+        Zangano zangano = new Zangano();
+        zangano.setCantidadMineralExtraidoPorTurno(10);
+        zangano.recibirMineral(nodoMineral.getMineral());
+        zangano.avanzarTurno();
+        Assertions.assertEquals(0, zangano.devolverMineralExtraido());
+        zangano.avanzarTurno();
+        Assertions.assertEquals(0, zangano.devolverMineralExtraido());
+
+    }
+
+    @Test
+    @DisplayName("Un nexo mineral extrae 10 unidades de mineral en 1 turno")
+    public void extraccionDeMineralConUnNexoMineralTest() {
+
+        NodoMineral nodoMineral = new NodoMineral(new Mineral(10));
+        NexoMineral nexoMineral = new NexoMineral();
+        nexoMineral.recibirMineral(nodoMineral.getMineral());
+        nexoMineral.setCantidadMineralExtraidoPorTurno(10);
+        nexoMineral.avanzarTurno();
+        Assertions.assertEquals(10, nexoMineral.devolverMineralExtraido());
+
+    }
+
+    @Test
+    @DisplayName("Un nexo mineral extrae 0 al extraer un nodo mineral agotado")
+    public void extraccionDeMineralConUnNexoMineralConNodoAgotadoTest() {
+
+        NodoMineral nodoMineral = new NodoMineral(new Mineral(0));
+        NexoMineral nexoMineral = new NexoMineral();
+        nexoMineral.recibirMineral(nodoMineral.getMineral());
+        nexoMineral.setCantidadMineralExtraidoPorTurno(10);
+        nexoMineral.avanzarTurno();
+        Assertions.assertEquals(0, nexoMineral.devolverMineralExtraido());
+        nexoMineral.avanzarTurno();
+        Assertions.assertEquals(0, nexoMineral.devolverMineralExtraido());
+
+    }
 
 }
