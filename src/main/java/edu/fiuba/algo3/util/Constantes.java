72db266d8d2c897af64f2a020abd0cd080eb7318
joeldevel <christianjoel.developer@gmail.com>
2022-11-10 16:29:51 -0300

    Se crean constantes para costos de contrucción protoss

---
+++ src/main/java/edu/fiuba/algo3/util/Constantes.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.util;
+
+public class Constantes {
+    public static final int COSTO_PILON = 100;
+    public static final int COSTO_ASIMILADOR = 100;
+    public static final int COSTO_ACCESO = 150;
+    public static final int COSTO_MINERAL_PUERTO = 150;
+    public static final int COSTO_GAS_PUERTO = 150;
+    public static final int COSTO_NEXO = 50;
+}
