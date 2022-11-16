ee59f333ee6597e3cfad57ceab9f59b184260a3c
LeoDuchen <82685204+LeoDuchen@users.noreply.github.com>
2022-11-16 00:38:10 -0300

    Caso 24 incompleto.

--- src/main/java/edu/fiuba/algo3/modelo/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mapa.java
@@ -1,25 +1,30 @@
 package edu.fiuba.algo3.modelo;
 
+import java.util.ArrayList;
+
 public class Mapa {
 
     private int cantidadDeBases;
+
+    private ArrayList<Base> bases;
 
     public Mapa(int unaCantidadDeBases) {
         this.cantidadDeBases = unaCantidadDeBases;
     }
 
-    private void calcularTamaño() {
+    private void calcularTamanio() {
 
     }
 
     private void crearBase() {
+        new ArrayList<Base>();
         for (int i = 1; i <= this.cantidadDeBases; ++i) {
-
+            bases.add(new Base());
         }
     }
 
     public boolean basesEstanEnExtremosOpuestos() {
-        // Comparar bases si son iguales.
+        // Comparar bases si son iguales o equidistantes.
 
         return true;
     }
