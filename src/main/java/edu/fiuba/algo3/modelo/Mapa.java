1055152733d7181cd57d74fd6d75579acd0e9455
LeoDuchen <82685204+LeoDuchen@users.noreply.github.com>
2022-11-15 22:51:04 -0300

    Caso 24 incompleto.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Mapa.java
@@ -0,0 +1,24 @@
+package edu.fiuba.algo3.modelo;
+
+public class Mapa {
+
+    private int cantidadDeBases;
+
+    public Mapa(int unaCantidadDeBases) {
+        this.cantidadDeBases = unaCantidadDeBases;
+    }
+
+    private void calcularTamaño() {
+
+    }
+
+    private void crearBase() {
+
+    }
+
+    public boolean basesEstanEnExtremosOpuestos() {
+        // Comparar bases si son iguales
+
+        return true;
+    }
+}
---
+++ src/main/test/edu/fiuba/algo3/entrega_2/MapaTest.java
@@ -0,0 +1,16 @@
+package edu.fiuba.algo3.entrega_2;
+
+import edu.fiuba.algo3.modelo.Mapa;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MapaTest {
+
+    @Test
+    void test01CreoUnaNuevaPartidaYLaBaseDelPrimerJugadorEstaAlExtremoOpuestoDeLaDelSegundo() {
+
+        Mapa mapa = new Mapa(2);
+
+        assertTrue(mapa.basesEstanEnExtremosOpuestos());
+    }
+}
