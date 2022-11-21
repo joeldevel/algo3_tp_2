26bf93b04ad04a7e272d9ae0c6f02286d6656af6
AlanValdevenito <avaldevenito@fi.uba.ar>
2022-11-21 19:16:10 -0300

    Mas correciones para los import. Más correciones para los parametros que reciben Volcan, NodoMineral y Zangano.

--- src/main/java/edu/fiuba/algo3/modelo/Areas/Base.java
+++ src/main/java/edu/fiuba/algo3/modelo/Areas/Base.java
@@ -1,8 +1,8 @@
 package edu.fiuba.algo3.modelo.Areas;
 
+import edu.fiuba.algo3.modelo.Recursos.Gas.Volcan;
 import edu.fiuba.algo3.modelo.Recursos.Minerales.NodoMineral;
 import edu.fiuba.algo3.modelo.Ubicacion;
-import Volcan;
 
 import java.util.ArrayList;
 
@@ -15,9 +15,9 @@ public class Base {
     private Ubicacion ubicacion;
 
     public Base(int x, int y) {
-        this.volcan = new Volcan(5000);
+        this.volcan = new Volcan();
         this.ubicacion = new Ubicacion(x, y);
-        nodosMinerales = new ArrayList<NodoMineral>() {{ add(new NodoMineral(2000)); add(new NodoMineral(2000)); add(new NodoMineral(2000));}};
+        nodosMinerales = new ArrayList<NodoMineral>() {{ add(new NodoMineral()); add(new NodoMineral()); add(new NodoMineral());}};
     }
 
     public int obtenerX() {
