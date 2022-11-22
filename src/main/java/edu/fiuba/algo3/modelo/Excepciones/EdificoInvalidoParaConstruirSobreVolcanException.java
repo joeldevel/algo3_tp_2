d35fd9fe4eb21a462fe5cd06a82a8987225c5904
LeoDuchen <82685204+LeoDuchen@users.noreply.github.com>
2022-11-22 16:29:27 -0300

    Caso de uso 3 incompleto.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Excepciones/EdificoInvalidoParaConstruirSobreVolcanException.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.Excepciones;
+
+public class EdificoInvalidoParaConstruirSobreVolcanException extends RuntimeException {
+    public EdificoInvalidoParaConstruirSobreVolcanException() {
+
+    }
+}
