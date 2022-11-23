690c505b2681ec3d8ad0c24c2a91e7f2f68f6c1f
joeldevel <christianjoel.developer@gmail.com>
2022-11-23 18:21:49 -0300

    Test básico para verificar límite de suministros en protoss

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso29Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso29Test.java
@@ -1,4 +1,23 @@
 package edu.fiuba.algo3.entrega_3;
 
+import edu.fiuba.algo3.modelo.Jugador.JugadorProtoss;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CasoDeUso29Test {
+
+    @Test
+    @DisplayName("Un protoss con 200 de suministros no puede incrementarlo")
+    public void protossLlegaAlLimiteDeSuministrosTest() {
+        JugadorProtoss jugadorProtoss = new JugadorProtoss();
+        jugadorProtoss.incrementarMineral(4100);
+        for (int i = 0; i < 40; i++) {
+            jugadorProtoss.crearPilon();
+        }
+
+        Assertions.assertEquals(200, jugadorProtoss.cupo());
+        jugadorProtoss.crearPilon();
+        Assertions.assertEquals(200, jugadorProtoss.cupo());
+    }
 }
