4987eee98784ecd9b9f612ac40bfa7aac0bae34f
joeldevel <christianjoel.developer@gmail.com>
2022-11-23 10:02:45 -0300

    Tests básicos para verificar cupo al crear Dragones

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso26.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso26.java
@@ -13,14 +13,14 @@ public class CasoDeUso26 {
 
     @Test
     @DisplayName("Un protoss no puede construir un Zealot si no tiene al menos 2 de cupo en su casa")
-    public void protossSinRecursosTest() {
+    public void protossSinRecursosZealotTest() {
         JugadorProtoss jugadorProtoss = new JugadorProtoss();
         Assertions.assertThrows(SinCupoSuficienteException.class, () -> jugadorProtoss.crearZealot());
     }
 
     @Test
     @DisplayName("Un protoss puede construir un Zealot si tiene al menos 2 de cupo en su casa")
-    public void protossConRecursosTest() {
+    public void protossConRecursosZealotTest() {
         JugadorProtoss jugadorProtoss = new JugadorProtoss();
         jugadorProtoss.incrementarMineral(100);
         jugadorProtoss.crearPilon();
@@ -52,5 +52,47 @@ public class CasoDeUso26 {
         Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.ZEALOT) == 2);
         Assertions.assertThrows(SinCupoSuficienteException.class, () -> jugadorProtoss.crearZealot());
         Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.ZEALOT) == 2);
+    }
+
+    @Test
+    @DisplayName("Un protoss no puede construir un Dragon si no tiene al menos 3 de cupo en su casa")
+    public void protossSinRecursosDragonTest() {
+        JugadorProtoss jugadorProtoss = new JugadorProtoss();
+        Assertions.assertThrows(SinCupoSuficienteException.class, () -> jugadorProtoss.crearDragon());
+    }
+
+    @Test
+    @DisplayName("Un protoss puede construir un Dragon si tiene al menos 3 de cupo en su casa")
+    public void protossConRecursosDragonTest() {
+        JugadorProtoss jugadorProtoss = new JugadorProtoss();
+        jugadorProtoss.incrementarMineral(100);
+        jugadorProtoss.crearPilon();
+        Assertions.assertDoesNotThrow(jugadorProtoss::crearDragon);
+        Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.DRAGON) == 1);
+    }
+
+    @Test
+    @DisplayName("Un protoss puede construir dos Dragones si tiene al menos 6 de cupo en su casa")
+    public void protossConRecursosConstruyeDosDragonesTest() {
+        JugadorProtoss jugadorProtoss = new JugadorProtoss();
+        jugadorProtoss.incrementarMineral(200);
+        jugadorProtoss.crearPilon();
+        jugadorProtoss.crearPilon();
+        Assertions.assertDoesNotThrow(jugadorProtoss::crearDragon);
+        Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.DRAGON) == 1);
+        Assertions.assertDoesNotThrow(jugadorProtoss::crearDragon);
+        Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.DRAGON) == 2);
+    }
+
+    @Test
+    @DisplayName("Contruir Dragones reduce la cantidad de cupos disponibles")
+    public void protossConRecursosConstruyeUnDragonYNoPuedeConstruirUnSegundoTest() {
+        JugadorProtoss jugadorProtoss = new JugadorProtoss();
+        jugadorProtoss.incrementarMineral(100);
+        jugadorProtoss.crearPilon();
+        Assertions.assertDoesNotThrow(jugadorProtoss::crearDragon);
+        Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.DRAGON) == 1);
+        Assertions.assertThrows(SinCupoSuficienteException.class, () -> jugadorProtoss.crearDragon());
+        Assertions.assertTrue(jugadorProtoss.cantidadDeUnidades(UNIDADES_PROTOSS.DRAGON) == 1);
     }
 }
