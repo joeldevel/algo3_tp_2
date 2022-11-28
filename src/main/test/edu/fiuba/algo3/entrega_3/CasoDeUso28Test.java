a75bef60b9dac48bfcba702af63bdb4914157de0
LeoDuchen <82685204+LeoDuchen@users.noreply.github.com>
2022-11-28 15:57:09 -0300

    Agrego un test al caso de uso 28.

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso28Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso28Test.java
@@ -2,6 +2,7 @@ package edu.fiuba.algo3.entrega_3;
 
 import edu.fiuba.algo3.modelo.Unidades.UnidadesProtoss.Zealot;
 import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.AmoSupremo;
+import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.Mutalisco;
 import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.Zerling;
 import org.junit.jupiter.api.Test;
 
@@ -36,5 +37,20 @@ public class CasoDeUso28Test {
 
         // Assert
         assertEquals(56, zealot.escudoRestante());
+    }
+
+    @Test
+    void test03UnAmoSupremoRevelaAUnZealotYAlAtacarloUnMutaliscoElEscudoDelZealotDisminuye(){
+        // Arrange
+        AmoSupremo amoSupremo = new AmoSupremo();
+        Mutalisco mutalisco = new Mutalisco();
+        Zealot zealot = new Zealot();
+
+        // Act
+        amoSupremo.revelar(zealot);
+        mutalisco.atacar(zealot);
+
+        // Assert
+        assertEquals(51, zealot.escudoRestante());
     }
 }
