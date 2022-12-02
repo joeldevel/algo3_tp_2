d772b11a8885faa9ac99f64fea80a22a72d42995
LeoDuchen <82685204+LeoDuchen@users.noreply.github.com>
2022-12-02 17:42:45 -0300

    Actualizacion caso de uso 28.

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso28Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso28Test.java
@@ -1,16 +1,19 @@
 package edu.fiuba.algo3.entrega_3;
 
+import edu.fiuba.algo3.modelo.Excepciones.RevelableFueraDeRangoError;
 import edu.fiuba.algo3.modelo.Jugador.JugadorProtoss;
 import edu.fiuba.algo3.modelo.Jugador.JugadorZerg;
 import edu.fiuba.algo3.modelo.Recursos.Recursos;
 import edu.fiuba.algo3.modelo.Ubicacion;
 import edu.fiuba.algo3.modelo.Unidades.UnidadesProtoss.Zealot;
 import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.AmoSupremo;
+import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.Hidralisco;
 import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.Mutalisco;
 import edu.fiuba.algo3.modelo.Unidades.UnidadesZerg.Zerling;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CasoDeUso28Test {
 
@@ -62,5 +65,36 @@ public class CasoDeUso28Test {
 
         // Assert
         assertEquals(51, zealot.escudoRestante());
+    }
+
+    @Test
+    void test04UnAmoSupremoRevelaAUnZealotYAlAtacarloUnHidraliscoElEscudoDelZealotDisminuye(){
+        // Arrange
+        JugadorZerg jugadorZerg = new JugadorZerg("Zerg", "Rojo", new Recursos(1000,1000));
+        AmoSupremo amoSupremo = new AmoSupremo(new Ubicacion(0,0), jugadorZerg);
+        Hidralisco hidralisco = new Hidralisco(new Ubicacion(0,0), jugadorZerg);
+        JugadorProtoss jugadorProtoss = new JugadorProtoss("Protoss", "Azul", new Recursos(1000,1000));
+        Zealot zealot = new Zealot(new Ubicacion(0,0), jugadorProtoss); // Unidad de tierra
+
+        // Act
+        amoSupremo.revelar(zealot);
+        hidralisco.atacar(zealot);
+
+        // Assert
+        assertEquals(50, zealot.escudoRestante());
+    }
+
+    @Test
+    void test05UnAmoSupremoIntentaRevelarAUnZealotPeroEstaFueraDeRangoYTiraUnError(){
+        // Arrange
+        JugadorZerg jugadorZerg = new JugadorZerg("Zerg", "Rojo", new Recursos(1000,1000));
+        AmoSupremo amoSupremo = new AmoSupremo(new Ubicacion(10,10), jugadorZerg);
+        JugadorProtoss jugadorProtoss = new JugadorProtoss("Protoss", "Azul", new Recursos(1000,1000));
+        Zealot zealot = new Zealot(new Ubicacion(0,0), jugadorProtoss); // Unidad de tierra
+
+        // Act and Assert
+        assertThrows(RevelableFueraDeRangoError.class,()->{
+            amoSupremo.revelar(zealot);
+        });
     }
 }
