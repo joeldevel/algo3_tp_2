71bad4f73f8f87bb3713c2e44458ac9922db2f8c
AlanValdevenito <avaldevenito@fi.uba.ar>
2022-12-14 16:49:31 -0300

    JavaFX

--- src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso32Test.java
+++ src/main/test/edu/fiuba/algo3/entrega_3/CasoDeUso32Test.java
@@ -1,5 +1,7 @@
 package edu.fiuba.algo3.entrega_3;
 
+import edu.fiuba.algo3.modelo.AlgoStar;
+import edu.fiuba.algo3.modelo.Excepciones.AlgoStarFinalizadoException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -9,9 +11,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CasoDeUso32Test {
 
     @Test
-    public void test01AlgoStarInicializadoEsFinalizado() {
-        //AlgoStar juego = new AlgoStar();
-        //juego.finalizarAlgoStar();
-        //assertThrows(AlgoStarFinalizadoException.class, () -> juego.avanzarTurno());
+    public void test01AlgoStarAvanzaDiezTurnosSinQueSeConstruyanEdificiosYSeFinaliza() {
+        // Arrange
+        AlgoStar juego = new AlgoStar();
+        juego.crearJugador("JugadorZerg", "Azul", "Zerg");
+        juego.crearJugador("JugadorProtoss", "Rojo", "Protoss");
+
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+        juego.avanzarTurno();
+
+        assertThrows(AlgoStarFinalizadoException.class, () -> juego.avanzarTurno());
     }
 }
